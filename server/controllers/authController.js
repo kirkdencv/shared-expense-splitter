9cb3d09fa6d070c57142d59727c6994688caf8e9
Aiyennn <146877043+Aiyennn@users.noreply.github.com>
2025-07-21 09:45:34 +0800

    cleanup

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const JWT_EXPIRY = '7d';
-const saltRounds = 10;
 
 const registerUser = async (req, res) => {
       try {
